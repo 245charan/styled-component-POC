c835779d32958cc4d2dd483f08aaf5870384e942
charan <charan.netha@acsicorp.com>
2022-03-28 18:28:48 +0530

    Add existing file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { ThemeProvider } from "styled-components";
+import { Container } from "./components/styles/Container.styled";
+import GlobalStyles from "./components/styles/Global";
+import Header from "./components/Header";
+import Card from "./components/Card";
+import content from "./content";
+import Footer from "./components/Footer";
 function App() {
+  const theme = {
+    colors: {
+      header: "#ebfbff",
+      body: "#fff",
+      footer: "#003333",
+    },
+    mobile: "768px",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Container>
+          {content.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
+        </Container>
+        <Footer />
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import { StyledCard } from "./styles/Card.styled";
+
+function Card({ item: { id, title, body, image } }) {
+  return (
+    <StyledCard layout={id % 2 === 0 && "row-reverse"}>
+      <div>
+        <div>
+          <h2>{title}</h2>
+          <p>{body}</p>
+        </div>
+        <div>
+          <img src={`./images/${image}`} alt="" />
+        </div>
+      </div>
+    </StyledCard>
+  );
+}
+
+export default Card;
